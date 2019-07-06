fb55d5d54777ad58ab8d270b7295f89cc050e199
Michal Satlawa <mi.satlawa@gmail.com>
2019-07-07 00:46:43 +0200

    add enviroment class

--- configs.py
+++ configs.py
@@ -4,13 +4,14 @@ class AttrDict(dict):
   __getattr__ = dict.__getitem__
   __setattr__ = dict.__setitem__
 
-STATE_SIZE = 20 * 24
-ACTION_SIZE = 6
+STATE_SIZE = 6*6*9
+ALLOWED_MOVES = ["L", "R", "U", "D", "F", "B",
+                 "L'", "R'", "U'", "D'", "F'", "B'"]
 
 model_config = AttrDict(
   {
     "state_size": STATE_SIZE,
-    "action_size": ACTION_SIZE,
+    "action_size": len(ALLOWED_MOVES),
     "hidden_layers": [100, 100, 100],
     "dtype": tf.float32,
     "optimizer": tf.train.GradientDescentOptimizer,
@@ -21,7 +22,7 @@ model_config = AttrDict(
 agent_config = AttrDict(
   {
     "state_size": STATE_SIZE,
-    "action_size": ACTION_SIZE,
+    "action_size": len(ALLOWED_MOVES),
     "memory_size": 10000,
     "exploration_rate": 1,
     "gamma": 0.9
@@ -30,9 +31,11 @@ agent_config = AttrDict(
 
 environment_config = AttrDict(
   {
+    "state_size": STATE_SIZE,
+    "allowed_moves": ALLOWED_MOVES,
     "step_reward": -1,
     "success_reward": 20,
     "fail_reward": -20,
-    "max_steps": 50
+    "max_steps": 50,
   }
 )
