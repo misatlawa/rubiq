c71b601610280ac1935cae701644df9f7ff710e1
Michal Satlawa <mi.satlawa@gmail.com>
2019-07-07 22:53:10 +0200

    modify rewards so the qvalue is bounded to sigmoid values

--- configs.py
+++ configs.py
@@ -5,17 +5,18 @@ class AttrDict(dict):
   __setattr__ = dict.__setitem__
 
 STATE_SIZE = 6*6*9
-ALLOWED_MOVES = ["L", "R", "U", "D", "F", "B",
+ALLOWED_MOVES = ["L",  "R",  "U",  "D",  "F",  "B",
                  "L'", "R'", "U'", "D'", "F'", "B'"]
 
 model_config = AttrDict(
   {
     "state_size": STATE_SIZE,
     "action_size": len(ALLOWED_MOVES),
-    "hidden_layers": [100, 100, 100],
+    "hidden_layers": [STATE_SIZE, 200, 100],
     "dtype": tf.float32,
     "optimizer": tf.train.GradientDescentOptimizer,
     "learning_rate": 1e-3,
+    "batch_size": 200
   }
 )
 
@@ -24,7 +25,8 @@ agent_config = AttrDict(
     "state_size": STATE_SIZE,
     "action_size": len(ALLOWED_MOVES),
     "memory_size": 10000,
-    "exploration_rate": 1,
+    "max_exploration_rate": 0.9,
+    "min_exploration_rate": 0.1,
     "gamma": 0.9
   }
 )
@@ -33,9 +35,9 @@ environment_config = AttrDict(
   {
     "state_size": STATE_SIZE,
     "allowed_moves": ALLOWED_MOVES,
-    "step_reward": -1,
-    "success_reward": 20,
-    "fail_reward": -20,
-    "max_steps": 50,
+    "step_reward": -0.1,
+    "success_reward": 1,
+    "fail_reward": -1,
+    "max_steps": 5,
   }
 )
