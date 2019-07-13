4d50bf81f5811b50416ef2637b7aaedb25560b7c
Michal Satlawa <mi.satlawa@gmail.com>
2019-07-13 15:53:27 +0200

    add doubleDQN model

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from os import path, strerror
+from attrdict import AttrDict
 import errno
 
 import tensorflow as tf
@@ -27,15 +28,14 @@ class Sequential:
       self.loss = self._loss()
       self.train_op = self._train_op()
 
-    cpu_config = tf.ConfigProto(
-        device_count = {'GPU': 0}
-    )
-    #self.cpu_session = tf.Session(config=cpu_config)
     self.session = tf.Session()
-    self.saver = tf.train.Saver()
+    self.saver = tf.train.Saver(max_to_keep=10)
+
+    self.summary = tf.summary.merge_all()
+    self.writer = tf.summary.FileWriter(config.logdir, self.session.graph)
+
     self.init = tf.global_variables_initializer()
     self.session.run(self.init)
-    #self.cpu_session.run(self.init)
 
   def _states(self):
     return tf.placeholder(
@@ -66,7 +66,6 @@ class Sequential:
         num_outputs=output_size,
         activation_fn=tf.nn.tanh
       )
-
     return tf.contrib.layers.fully_connected(
       inputs=input_,
       num_outputs=self.config.action_size,
@@ -125,3 +124,46 @@ class Sequential:
         strerror(errno.ENOENT),
         path_
       )
+
+class DoubleDQN(Sequential):
+  def __init__(self, config):
+    super().__init__(config)
+    cpu_config = tf.ConfigProto(
+        device_count={'GPU': 0}
+    )
+    self.cpu_session = tf.Session(config=cpu_config)
+    self.cpu_session.run(self.init)
+
+  def act(self, state):
+    action = self.cpu_session.run(
+      fetches=(self.action_prediction),
+      feed_dict={self.states: [state]}
+    )
+    return action[0]
+
+  def update_weights(self):
+    trainable_variables = tf.trainable_variables()
+    variable_values = self.session.run(
+      trainable_variables
+    )
+    self.cpu_session.run(
+      [
+        variable.assign(value)
+        for variable, value in zip(trainable_variables, variable_values)
+      ]
+    )
+
+  def train(self, states, actions, values):
+    loss, step, summary, _ = self.session.run(
+      fetches=(self.loss, self.global_step, self.summary, self.train_op),
+      feed_dict={
+        self.states: states,
+        self.actions: actions,
+        self.values: values
+      }
+    )
+    self.writer.add_summary(summary, global_step=step)
+    if not step % self.config.weight_update_interval:
+      self.update_weights()
+
+    return loss
