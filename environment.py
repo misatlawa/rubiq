c641724eb027fc8ed61fdb3ee32bbb30c936d3d2
Michal Satlawa <mi.satlawa@gmail.com>
2019-07-16 14:48:58 +0200

    add reset method

--- environment.py
+++ environment.py
@@ -1,4 +1,3 @@
-import numpy as np
 from cube.cube import Cube
 
 class RubiksCubeEnvironment:
@@ -6,7 +5,6 @@ class RubiksCubeEnvironment:
   def __init__(self, config):
     self.config = config
     self.cube = Cube()
-    self.counter = 0
 
   def is_solved(self):
     return self.cube.solved
@@ -17,17 +15,17 @@ class RubiksCubeEnvironment:
   def scramble(self, difficulty=26):
     self.cube.shuffle(moves=difficulty)
 
+  def reset(self):
+    self.cube.reset()
+
   def __call__(self, action_id=None):
     if action_id is not None:
       self.cube.move(self.config.allowed_moves[action_id])
 
-    is_terminal = self.is_solved()
-    if self.counter < self.config.max_steps and is_terminal:
+    is_solved = self.is_solved()
+    if is_solved:
       reward = self.config.success_reward
-    elif self.counter < self.config.max_steps:
-      reward = self.config.step_reward
     else:
-      reward = self.config.fail_reward
-      is_terminal = True
-    self.counter += 1
-    return reward, self.encoded_state(), is_terminal
+      reward = self.config.step_reward
+
+    return reward, self.encoded_state(), is_solved
