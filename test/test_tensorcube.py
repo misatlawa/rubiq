29fb347bcdf8eda95e2e74b26727cd66e58e1a17
Lepakko <mixer59@wp.pl>
2019-07-24 23:14:49 +0200

    add some simple tests for TensorCube

--- test/test_tensorcube.py
+++ test/test_tensorcube.py
@@ -12,3 +12,26 @@ class TensorCubeTest(unittest.TestCase):
     self.assertFalse(s.all())
     s, r = c.action(list(repeat(1, 100)), r)
     self.assertTrue(s.all())
+
+  def test_b_few_moves(self):
+    c = TensorCube()
+    r = list(repeat(get_zero_cube(), 100))
+
+    for move in filter(lambda x: x % 2 == 0, range(12)):
+      s, r = c.action(list(repeat(move, 100)), r)
+    self.assertFalse(s.all())
+
+    for move in filter(lambda x: x % 2 != 0, range(12)):
+      s, r = c.action(list(repeat(move, 100)), r)
+    self.assertTrue(s.all())
+
+  def test_c_RUru(self):
+    c = TensorCube()
+    sequence = [0, 8, 1, 9]
+    r = list(repeat(get_zero_cube(), 100))
+
+    for i in range(1, 7):
+      for move in sequence:
+        s, r = c.action(list(repeat(move, 100)), r)
+      self.assertEqual(s.all(), i % 6 == 0)
+
